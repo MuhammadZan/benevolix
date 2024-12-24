7b2e6c19c492cbb8efeafb27839b5ab165949abe
MuhammadZan <zain@pebitech.com>
2024-12-24 23:26:50 +0500

    benevlix start

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import { AppProps } from "next/app";
+import "@/styles/globals.css"
+const _app = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default _app;
